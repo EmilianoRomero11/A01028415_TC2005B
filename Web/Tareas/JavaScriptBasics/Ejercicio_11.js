8460ada5b8ff8084c09792ab35c3d0d638a1c5a9
EmilianoRomero11 <159821408+EmilianoRomero11@users.noreply.github.com>
2024-02-26 14:15:30 -0600

    Delete Web/Tareas/JavaScriptBasics directory
    The name of the directory was incorrect

--- Web/Tareas/JavaScriptBasics/Ejercicio_11.js
+++
@@ -1,8 +0,0 @@
-function ordenAlfabetico(lista){
-    let resultado = lista.sort()
-    return resultado
-}
-
-let lista = ["Beta", "Gamma", "Epsilon", "Alfa", "Sigma", "Zeta"]
-let ordenada = ordenAlfabetico(lista)
-console.log(ordenada)
